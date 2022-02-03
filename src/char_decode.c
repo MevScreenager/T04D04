db3c1fbb4fde52a03cc4120333f7608241f3b394
unnamed <arrynpal@am-f2.msk.21-school.ru>
2022-02-03 20:34:17 +0300

    char_decode.c

---
+++ src/char_decode.c
@@ -0,0 +1,18 @@
+#include<stdio.h>i
+#include<math.h>
+
+int main(){
+	int d;
+	scanf("%d", &d);
+	if (d == 0){
+		char s;
+		scanf("%s", &s);
+	}if (d == 1){
+	}else{
+		printf("n/a");
+		return -1;
+	}
+
+	
+	
+}
